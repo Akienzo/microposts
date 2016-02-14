66ab9ae1bfd94504a9dad086da8656455726efc7
akienzo <ringstyle2@gmail.com>
2016-02-14 08:09:21 +0000

    finish8-6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   # before_action :collect_user, only: [:edit, :update]  
+  before_action :set_user,
+                only: [:show, :edit, :update, :followings, :followers]
+  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :authenticate!, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.order(created_at: :desc)  #ユーザーに紐付いたマイクロポストを作成日時が新しいものから取得し、@micropostsに代入しています。
@@ -31,7 +36,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Edit to the Sample App!"
-      redirect_to @user　
+      redirect_to @user
     else
       render 'edit'
     end
@@ -43,6 +48,27 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :area, :password,:password_confirmation)
   end
   
+    # get parameters for sinup
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # get parameters for profile editing
+  def user_profile
+    params.require(:user).permit(:name, :email, :profile, :area, :password, :password_confirmation)
+  end
+
+  # get a user instance with :id parameter
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # check current_user is editing self ?
+  def authenticate!
+    if @user != current_user
+      redirect_to root_url, flash: { alert: "不正なアクセス" }
+    end
+  end
   # def collect_user
   # user = User.find(params[:id]) redirect_to(root_url) if user != current_user
   # end
